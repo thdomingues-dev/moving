0924785737fb17a751cb528eea349ef9c1d09279
Thales Domingues <thdomingues.dev@gmail.com>
2021-03-03 01:54:49 -0400

    Homepage Implementation

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/thdomingues-dev.png" alt="Thales Domingues" />
+      <div>
+        <strong>Thales Domingues</strong>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
